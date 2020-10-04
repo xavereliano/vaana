94950577863ca74db20a1ae9a7b6acd8350feea3
xavereliano <53742706+xavereliano@users.noreply.github.com>
2020-10-05 00:06:31 +0300

    Add files via upload

---
+++ js/fixed-header.js
@@ -0,0 +1,15 @@
+// Desktop Fixed-Header
+$(document).ready(function() {
+
+    $('.burger').click(function(){
+        $('header').toggleClass('clicked');
+    });
+
+    $('nav ul li').click(function(){
+        $('nav ul li').removeClass('selected');
+        $('nav ul li').addClass('notselected');
+        $(this).toggleClass('selected');
+        $(this).removeClass('notselected');
+    });
+    
+});
